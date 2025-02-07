1517f85469fe930fb4a2173b56c0366d25c5cdf5
nithishkarthick <nknithish0708@gmail.com>
2025-02-07 15:52:19 +0530

    mic 44

--- user-service/wait-for-it.sh
+++ user-service/wait-for-it.sh
@@ -1,28 +1,22 @@
-#!/bin/bash
-# wait-for-it.sh
+#!/usr/bin/env bash
+# wait-for-it.sh: Wait for a service to become available
 
-# wait-for-it.sh waits for a service to be ready and then runs a command
-# Usage: wait-for-it.sh host:port -- command args...
-
-TIMEOUT=15
-QUIET=0
-HOST="$1"
-PORT="$2"
-CMD="${@:4}"
-
+TIMEOUT=30
+HOST=$1
+PORT=$2
 shift 2
+CMD=$@
 
 echo "Waiting for $HOST:$PORT to be available..."
 
-while ! nc -z "$HOST" "$PORT"; do
-  if [ $QUIET -eq 0 ]; then
-    echo "Waiting..."
-  fi
-  sleep 1
+# Wait for the service to be ready
+until nc -z -v -w30 $HOST $PORT
+do
+  echo "$HOST:$PORT is unavailable - sleeping"
+  sleep 5
 done
 
-if [ $QUIET -eq 0 ]; then
-  echo "$HOST:$PORT is available!"
-fi
+echo "$HOST:$PORT is now available"
 
+# Execute the command
 exec $CMD
